0e2466adecf81c1cd66c7b06679607a92bda8f96
victormodupe <victormodupe007@gmail.com>
2024-04-07 13:51:28 +0000

    git push nigga fourth or fifth not sure

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,10 @@ def safe_print_list(my_list=[], x=0):
     index = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print(my_list[index], end="")
             index += 1
         except IndexError:
             break
+
     print("")
-    return(index)
+    return index
